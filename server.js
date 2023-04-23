339e8ed755c5f5b68660a397bb7c56118e53504f
Conor Odoemene <conor99055@gmail.com>
2023-04-23 22:10:58 +0100

    Use environment variable for key.pem

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const https = require('https');
 const fs = require('fs');
-const merchantModel = require("./models/merchantModels")
+const Meal = require('./models/mealsModels.js');
 const mongoose = require('mongoose')
 const { body, validationResult } = require('express-validator');
 
 
 
 // Loading in the certificate and private key
-const privateKey = fs.readFileSync('key.pem', 'utf8');
+const key = process.env.KEY_PEM;
 const certificate = fs.readFileSync('cert.pem', 'utf8');
 const ca = fs.readFileSync('cert.pem', 'utf8');
 
@@ -34,101 +34,100 @@ app.use(express.json()) //allows app to understand JSON
 
 //routes
 app.get('/', (req, res)=> {
-    res.send('Hello Merchant API')
+    res.send('Hello Meals API')
 })
 
 
-//returns all merchants
-app.get('/merchants', async(req, res)=>{
-    try{
-        const merchants = await merchantModel.find({}).select('-revenue');; //empty curly brackets is all products
-        res.status(200).json(merchants)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
-
-
-//returns individual merchant
-app.get('/merchants/:merchantId', async(req, res)=>{
-    try{
-        const merchant = await merchantModel.findOne({ merchantId: req.params.merchantId }).select('-revenue');; 
-        if (!merchant) {
-            return res.status(404).json({ message: 'Merchant not found' });
-        }
-        res.status(200).json(merchant)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+app.get('/meals', async (req, res) => {
+  try {
+    const meals = await Meal.find({});
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 
 
-app.post('/merchants',   [
-  body('latitude').isNumeric().withMessage('Latitude must be a number'),
-  body('longitude').isNumeric().withMessage('Longitude must be a number'),
-  body('merchantId').isInt().withMessage('Merchant ID must be an integer'),
-  body('merchantName').trim().escape().isLength({ min: 1 }).withMessage('Merchant name must not be empty'),
-  body('revenue').isNumeric().withMessage('Revenue must be a number')
-], async(req, res)=>{
-    try{
-        const merchant = await merchantModel.create(req.body)
-        res.status(200).json(merchant);
-    } 
-    catch(error){
-        console.log(error.message)
+//returns individual meal
+app.get('/meals/:id', async (req, res) => {
+  try {
+    const meal = await Meal.findById(req.params.id);
+    if (!meal) {
+      return res.status(404).json({ message: 'Meal not found' });
     }
-})
+    res.status(200).json(meal);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+
+app.post('/meals', async (req, res) => {
+  try {
+    const meal = await Meal.create(req.body);
+    res.status(201).json(meal);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
+app.post('/meals/bulk', async (req, res) => {
+  try {
+    const meals = req.body.meals;
+    const insertedMeals = await Meal.insertMany(meals);
+    res.status(201).json(insertedMeals);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 
 //update merchants   / Broken Access Control (A5)
-app.put('/merchants/:merchantId',   [
-  body('latitude').isNumeric().withMessage('Latitude must be a number'),
-  body('longitude').isNumeric().withMessage('Longitude must be a number'),
-  body('merchantId').isInt().withMessage('Merchant ID must be an integer'),
-  body('merchantName').trim().escape().isLength({ min: 1 }).withMessage('Merchant name must not be empty'),
-  body('revenue').isNumeric().withMessage('Revenue must be a number')
+app.put('/meals/:id', [
+  body('name').trim().escape().isLength({ min: 1 }).withMessage("Please enter the meal's name"),
+  body('prep_time').isNumeric().withMessage("Please enter the meal's prep time"),
+  body('calories').isNumeric().withMessage("Please enter the meal's calories"),
+  body('origin').trim().escape().isLength({ min: 1 }).withMessage("Please enter the meals origin"),
+  body('continent').trim().escape().isLength({ min: 1 }).withMessage("Please enter the meals continent of origin")
 ], async (req, res) => {
-    try {
-      const { merchantId } = req.params;
-      const updatedMerchant = await merchantModel.findOneAndUpdate({ merchantId }, req.body, { new: true });
-      if (!updatedMerchant) {
-        return res.status(404).json({ message: 'Merchant not found' });
-      }
-      res.status(200).json(updatedMerchant);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+  try {
+    const meal = await Meal.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!meal) {
+      return res.status(404).json({ message: 'Meal not found' });
     }
-  });
+    res.status(200).json(meal);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 
 
-  app.delete('/merchants/:merchantId', async (req, res) => {
-    try {
-      const { merchantId } = req.params;
-      const deletedMerchant = await merchantModel.findOneAndDelete({ merchantId });
-      if (!deletedMerchant) {
-        return res.status(404).json({ message: 'Merchant not found' });
-      }
-      res.status(200).json({ message: 'Merchant deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+
+app.delete('/meals/:id', async (req, res) => {
+  try {
+    const meal = await Meal.findByIdAndDelete(req.params.id);
+    if (!meal) {
+      return res.status(404).json({ message: 'Meal not found' });
     }
-  });
+    res.status(200).json({ message: 'Meal deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 
 
-
-  
 
 
 
 
 
 //connecting to database 
-mongoose.connect('mongodb+srv://conor990:Tallaght@merchantapi.sevbnez.mongodb.net/merchantAPI?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://conor990:Tallaght1!@mealsapi.pnm5tav.mongodb.net/test')
 .then(()=> {
     console.log('connected to MongoDB')
     //server listening for incoming requests
